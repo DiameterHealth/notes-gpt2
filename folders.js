ea94c57b8c413e97abb15b01d32d107691fd2431
John D'Amore <jddamore@gmail.com>
2020-06-30 17:02:15 -0400

    Pushing first pass at all data

--- folders.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-for (let i = 0; i < 640; i++) {
-  let num = '';
-  if (i < 10) num = `00${i}`; 
-  else if (i < 100) num = `0${i}`; 
-  else num = `${i}`; 
-  fs.mkdirSync(`./note${num}`);
-} 
